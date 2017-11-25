e50a8a0835848acfaa35a0891d492564238c265c
armaangoel78 <armaangoel78@gmail.com>
2017-11-25 17:52:43 -0500

    fixed security issue, will break code

--- sapphire/sapphire/settings.py
+++ sapphire/sapphire/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!&jhwwd*#!i571m03y8yg*!k@^ev_=3wc0=p1g@sxcf%-2krqq'
+SECRET_KEY = os.environ['SKEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
