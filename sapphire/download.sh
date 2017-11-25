e95c2ab2139b762d183db5513e3f0101eb14633b
armaangoel78 <armaangoel78@gmail.com>
2017-11-25 12:11:24 -0500

    added download and run scripts

---
+++ sapphire/download.sh
@@ -0,0 +1,8 @@
+#the following script will download what you need to get started with this project, you will however, also need to install python 3 to for the following donloads to work. Checkout our website, chscodeforchange.wordpress.com/downloads to see how to download this
+
+echo
+echo Starting downloads
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pip3 install django;
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pip3 install djangorestframework;
