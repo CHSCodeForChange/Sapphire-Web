e4fccfd4f8fa617a085e859c63d2047a5af288cf
jpaav <joepaavola@gmail.com>
2018-04-08 00:21:21 -0400

    Updates

--- sapphire/sapphire/settings.py
+++ sapphire/sapphire/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 
 TEMPLATES[0]['OPTIONS']['context_processors'].append("alerts.context_processors.getAlerts")
 
-WSGI_APPLICATION = 'sapphire.wsgi'
+# WSGI_APPLICATION = 'sapphire.wsgi'
 
 
 # Database
