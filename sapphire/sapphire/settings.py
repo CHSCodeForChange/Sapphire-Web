219d06c8ee2bd825c00f8bb5f1b4a5791863733a
jpaav <joepaavola@gmail.com>
2018-04-08 00:45:25 -0400

    Update settings.py

--- sapphire/sapphire/settings.py
+++ sapphire/sapphire/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 INSTALLED_APPS = [
     'crispy_forms',
+    'gunicorn',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
