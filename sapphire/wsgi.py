d02d1ef68a0fba8251b69f4a95bc5fa9ae5cab4e
jpaav <joepaavola@gmail.com>
2018-04-08 01:22:09 -0400

    Updated Procfile

--- sapphire/wsgi.py
+++ sapphire/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sapphire.sapphire.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sapphire.settings")
 
 application = get_wsgi_application()
 
