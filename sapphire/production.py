043215c18e99a46cacfb643906fda9f97a181b9f
Armaan Goel <armaangoel78@gmail.com>
2018-05-23 20:04:11 -0400

    try 2: testing signin email fix

--- sapphire/production.py
+++ sapphire/production.py
@@ -161,8 +161,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/home/'
 
-EMAIL_HOST_USER = 'testemail2081@gmail.com' #this is a testing account
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = 'sapphireappstaff@gmail.com' #this is a testing account
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'securestuff'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 EMAIL_PORT = 587
