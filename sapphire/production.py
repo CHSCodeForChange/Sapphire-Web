5f89253d7bf46c8c9f0a59b5ce7b8f497dece47b
Armaan Goel <armaangoel78@gmail.com>
2018-05-23 20:13:46 -0400

    try #4

--- sapphire/production.py
+++ sapphire/production.py
@@ -161,7 +161,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/home/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'sapphireappstaff@gmail.com' #this is a testing account
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
