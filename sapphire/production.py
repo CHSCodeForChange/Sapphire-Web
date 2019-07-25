93c768da7f8207de96ee4b4dc10e7f2078f065b3
Evan Kenyon <evan.kenyon123@gmail.com>
2019-07-25 19:54:01 -0400

    Change email used

--- sapphire/production.py
+++ sapphire/production.py
@@ -162,7 +162,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/home/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'sapphireappstaff@gmail.com' #this is a testing account
+EMAIL_HOST_USER = 'sapphireapplicationstaff@gmail.com' #this is a testing account
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
